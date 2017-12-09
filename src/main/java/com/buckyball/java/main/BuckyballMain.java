a04e80ebd48605286930d858efb338442babba65
liuweies <574613441@qq.com>
2017-12-09 23:02:34 +0800

    update config

---
+++ src/main/java/com/buckyball/java/main/BuckyballMain.java
@@ -0,0 +1,13 @@
+package com.buckyball.java.main;
+
+/**
+ * buckyball-java
+ * Author:把代码敲晕
+ * Date:2017/12/9
+ * Description:
+ */
+public class BuckyballMain {
+    public static void main(String[] args){
+        System.out.println("我是神仙");
+    }
+}
